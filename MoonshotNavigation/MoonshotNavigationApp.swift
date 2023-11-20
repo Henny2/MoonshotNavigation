5b4923401297fd40ff206c3ba3ebc083b2fe08a4
Henny2 <henrieke.baunack@web.de>
2023-11-19 19:47:35 -0800

    Initial Commit

---
+++ MoonshotNavigation/MoonshotNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotNavigationApp.swift
+//  MoonshotNavigation
+//
+//  Created by Henrieke Baunack on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
